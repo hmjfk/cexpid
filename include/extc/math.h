89fac27e31521c063413bb9172ea085081d4dc28
hmjfk <61924124+hmjfk@users.noreply.github.com>
2026-06-21 07:59:50 +0900

    add: math constant

--- include/extc/math.h
+++ include/extc/math.h
@@ -19,10 +19,14 @@
 #define EXTMATH_H
 #include<stdint.h>
 
-// deg → rad function
+// math constant
+constexpr double pi = 3.141592653589793238462643383279502;
+constexpr double e =  2.718281828459045235360287471352662;
+
+// deg -> rad function
 double rad(double value);
 
-// rad → deg function
+// rad -> deg function
 double deg(double value);
 
 // 角度変換関数
